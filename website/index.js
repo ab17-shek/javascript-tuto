ea431130a0229354b288c1fc492f0a780e8f32e5
Abhi <abhiagrawat29@gmail.com>
2026-01-08 20:33:49 +0530

    lesson 1 - basics of JS

---
+++ website/index.js
@@ -0,0 +1,7 @@
+console.log("Hello");
+console.log("I am new to JavaScript."); // This will log a message to the console
+
+document.getElementById("myH1").textContent = "Welcome to My Website"; // This will change the content of the h1 element
+document.getElementById("myP").textContent = "This is a simple webpage created using HTML, CSS, and JavaScript."; // This will change the content of the p element
+
+window.alert("Welcome to my website!"); // This will show an alert message when the page loads
